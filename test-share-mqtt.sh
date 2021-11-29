445eaecc3189fd168fe801a5e325b02b7ff8e4e8
David Ventura <95183870+DaVenMar@users.noreply.github.com>
2021-11-29 01:08:34 +0100

    Update test-share-mqtt.sh

--- test-share-mqtt.sh
+++ test-share-mqtt.sh
@@ -5,5 +5,5 @@ sudo docker build -f eclipse-mosquito/dockerfile -t mqtt-lb-server .
 sudo docker build -f mqtt-pub/dockerfile -t client-mqtt-pub .
 sudo docker build -f mqtt-sub/dockerfile -t client-mqtt-sub .
 #DEPLOY
-echo "deploy images"
+echo "Deploy docker containers"
 sudo docker-compose up -d --scale mqttclient=4
